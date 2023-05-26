a13010caa8183e10b6a98e8930e082935552ef01
Mila601 <mila_601@mail.ru>
2023-05-27 01:55:25 +0300

    fixed some output functions

--- src/bank_schedule/check_conditions.py
+++ src/bank_schedule/check_conditions.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 from datetime import datetime, timedelta
 import pandas as pd
+import numpy as np
 
 def condition_max_days_inc(df_opt_result, df_money_in, data):
     params_dict = data.get_params_dict()
@@ -17,17 +18,24 @@ def condition_max_days_inc(df_opt_result, df_money_in, data):
 
 
 
-def check_over_balance(df_opt_result,  data):
+def check_over_balance(df_opt_result,  data, list_TID_cluster = None):
     """
-    Функция принимает результаты оптимизации и выдает датафрейм с остатками на счетах в банкоматах на каждый день
+    Считаем баланc банкоматов на утро после инкасации и проверяем не переполнены ли они
     """
-    df_money_start= data.get_money_start()
     df_money_in = data.get_money_in()
+    df_money_start = data.get_money_start()
+
+    if list_TID_claster is not None:
+            df_money_in = df_money_in[df_money_in.TID.isin(list_TID_cluster)]
+            df_money_start = df_money_start[df_money_start.TID.isin(list_TID_cluster)]
+
     params_dict = data.get_params_dict()
     df_money = pd.DataFrame(index=df_money_in['TID'].unique())
-    for date in df_money_in.date.unique():
-        df_money[date] = df_money_in[df_money_in.date==date].set_index('TID')['money_in']
     df_money_start = df_money_start.set_index('TID').copy()
+    df_money[df_money_in.date.min()] = 0
+    for date in df_money_in.date.unique()[:-1]:
+        df_money[date+timedelta(days=1)] = df_money_in[df_money_in.date==date].set_index('TID')['money_in']
+
     df_money.columns = pd.to_datetime(df_money.columns)
 
     df_money_sum = df_money.copy()
@@ -38,9 +46,6 @@ def check_over_balance(df_opt_result,  data):
         for column_2 in df_money.columns[j+1:]:
             df_money_sum[column_2] = df_money_sum[column_2] + df_money[column]
 
-    date_start = df_money_sum.columns.min()
-    date_minus_1  = date_start - timedelta(days=1)
-    df_money_sum[date_minus_1] = df_money_start['money']
     df_money_sum = df_money_sum[df_money_sum.columns.sort_values()]
 
 
@@ -50,19 +55,10 @@ def check_over_balance(df_opt_result,  data):
 
     # обработаем инкасации
     list_date = np.sort(df_opt_result.date.unique())
-
     for day in tqdm(list_date):
         day = pd.to_datetime(day)
-        date_before = dict_num_to_date[dict_date_to_num[day]-1]
-        for j in range(dict_date_to_num[day], df_money_sum.shape[1]):
-            df_money_sum.loc[list(df_opt_result[df_opt_result.date==day]['TID']), dict_num_to_date[j]] -= df_money_sum.loc[list(df_opt_result[df_opt_result.date==day]['TID']), date_before]
-
-    list_date = df_opt_result.date.unique()
-    for day in tqdm(list_date):
-        day = pd.to_datetime(day)
-        df_money_sum.loc[list(df_opt_result[df_opt_result.date==day]['TID']), day] = 0
-
-    df_money_sum.drop([date_minus_1], axis=1, inplace=True)
+        for j in range(df_money_sum.shape[1]-1, dict_date_to_num[day]-1, -1):
+            df_money_sum.loc[list(df_opt_result[df_opt_result.date==day]['TID']), dict_num_to_date[j]] -= df_money_sum.loc[list(df_opt_result[df_opt_result.date==day]['TID']), day]
     assert sum((df_money_sum > params_dict['max_money']).sum()) == 0, 'alarm, max_money in ATM is too big'
 
     return df_money_sum       
