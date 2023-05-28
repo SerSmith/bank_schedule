a3d2c029b8867f6b4fdb13349ab8ca35dd5993cb
Мошков Николай Евгеньевич <moshkovne@sibur.ru>
2023-05-28 06:20:17 +0300

    scheduler added

--- src/bank_schedule/ortools_tsp.py
+++ src/bank_schedule/ortools_tsp.py
@@ -3,11 +3,14 @@
 from typing import List, Tuple
 
 import pandas as pd
-
-from bank_schedule.data import distances_matrix_from_dataframe
+import numpy as np
 
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
+
+from bank_schedule.data import distances_matrix_from_dataframe
+from bank_schedule import scheduler
+from bank_schedule.data import Data
 
 
 def create_data_model(distance_matrix: List[List[int]],
@@ -124,6 +127,19 @@ def solve_tsp_ortools(tids_list: List[int],
 
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    # PATH_CHEAPEST_ARC 4661.13 minutes
+    # PATH_MOST_CONSTRAINED_ARC 4725.2 minutes
+    # SAVINGS 4640.44 minutes
+    # SWEEP No solution found !
+    # CHRISTOFIDES 4584.5 minutes <---
+    # BEST_INSERTION No solution found !
+    # PARALLEL_CHEAPEST_INSERTION 4645.21 minutes
+    # SEQUENTIAL_CHEAPEST_INSERTION 4645.21 minutes
+    # LOCAL_CHEAPEST_INSERTION 4643.61 minutes
+    # LOCAL_CHEAPEST_COST_INSERTION 4643.61 minutes
+    # GLOBAL_CHEAPEST_ARC 4617.79 minutes
+    # LOCAL_CHEAPEST_ARC 4578.29 minute <---
+    # FIRST_UNBOUND_MIN_VALUE 4707.0 minutes
     search_parameters.first_solution_strategy = (
         routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC) # pylint: disable=no-member
 
@@ -137,23 +153,109 @@ def solve_tsp_ortools(tids_list: List[int],
         else:
             print('No solution found !')
 
-    if solution:
-        steps, steps_times, summary_time = get_route_and_time(manager, routing, solution)
-        steps = [tids_list[i] for i in steps]
-        return steps[:-1], steps_times[:-1], summary_time
+    if not solution:
+        raise ValueError('No solution found !')
 
-    return [], [], -1
+    steps, steps_times, summary_time = get_route_and_time(manager, routing, solution)
+    steps = [tids_list[i] for i in steps]
+    return steps[:-1], steps_times[:-1], summary_time
+
+
+def get_best_route(loader: Data,
+                   tids_list: List[int],
+                   n_iterations: int=10) -> Tuple[List[int], List[int], float]:
+    """Возвращает список банкоматов, которые нужно обслужить
+    в порядке оптимальног времени в пути, времена убытия из них
+
+    Args:
+        tids_list (List[int]): список банкоматов для обслуживания
+        distances_df (pd.DataFrame): датафрейм расстояний
+        n_iterations (int, optional): сколько попыток построения маршрута сделать. Defaults to 10.
+
+    Returns:
+        List[int]: _description_
+    """
+    distances_df = loader.get_distance_matrix()
+
+    best_sum_time = float('inf')
+    best_route = []
+    best_route_times = []
+    for _ in range(n_iterations):
+        depot = np.random.choice(tids_list)
+        route, route_times, sum_time = solve_tsp_ortools(tids_list,
+                                               distances_df,
+                                               depot)
+        if len(route) != len(set(route)):
+            print(route)
+            print(pd.Series(data=route).value_counts())
+            raise ValueError('Какие-то точки в маршруте посещаются не один раз')
+
+        if sum_time < best_sum_time:
+            best_sum_time = sum_time
+            best_route = route
+            best_route_times = route_times
+
+    return best_route, best_route_times, best_sum_time
+
+
+def optimize_routes(loader: Data,
+                    residuals: pd.DataFrame,
+                    atms_per_day: int=200,
+                    mandatory_selection_threshold: int=1,
+                    n_iterations: int=1,
+                    neighborhood_radius: float=15,
+                    max_route_time: float=720.0,
+                    mandatory_selection_col: str='days_to_deadline',
+                    tids_col: str='TID',
+                    use_greedy: bool=True):
+    """Находит количество машин, необходимое для обслуживания банкоматов
+    в день
+
+    Args:
+        loader (Data): класс загрузки данных
+        residuals (pd.DataFrame): данные об остатках, подготовленные для составления расписания
+        (см. scheduler.prepare_residual_to_schedule_creation)
+        atms_per_day (int, optional): _description_. Defaults to 150.
+        mandatory_selection_threshold (int, optional): _description_. Defaults to 1.
+        n_iterations (int, optional): _description_. Defaults to 1.
+        neighborhood_radius (float, optional): _description_. Defaults to 15.
+        max_route_time (float, optional): _description_. Defaults to 720.0.
+        mandatory_selection_col (str, optional): _description_. Defaults to 'days_to_deadline'.
+        tid_col (str, optional): _description_. Defaults to 'TID'.
+
+    Returns:
+        _type_: _description_
+    """
+
+    atms_df = scheduler.get_atms_for_today_collection(loader,
+                                                      residuals,
+                                                      atms_per_day,
+                                                      mandatory_selection_threshold,
+                                                      neighborhood_radius,
+                                                      mandatory_selection_col=mandatory_selection_col,
+                                                      tids_col=tids_col,
+                                                      use_greedy=use_greedy)
+    tids_for_collection = atms_df[tids_col].tolist()
+
+    route, route_time, route_time_sum = get_best_route(loader,
+                                                        tids_for_collection,
+                                                        n_iterations=n_iterations)
+
+    return route, route_time, route_time_sum / max_route_time
 
 
 if __name__ == '__main__':
+    from datetime import datetime
     from bank_schedule.data import Data
     from bank_schedule.constants import RAW_DATA_FOLDER
 
-    N_POINTS = 30
+    N_POINTS = 200
 
-    loader = Data(RAW_DATA_FOLDER)
-    distances_df = loader.get_distance_matrix()
+    myloader = Data(RAW_DATA_FOLDER)
+    distances_df = myloader.get_distance_matrix()
     tids_list = distances_df['Origin_tid'].sample(n=N_POINTS, random_state=0).tolist()
 
-    print(tids_list)
-    print( solve_tsp_ortools(tids_list, distances_df, depot=tids_list[0]) )
+    start = datetime.now()
+    res = solve_tsp_ortools(tids_list, distances_df, depot=tids_list[0], verbose=True)
+    end = datetime.now()
+    print(f'Time: {end - start}')
--- src/bank_schedule/scheduler.py
+++ src/bank_schedule/scheduler.py
@@ -1,12 +1,71 @@
 """Скрипты для построения расписания инкассации на текущий день
 """
 
-from typing import Dict, List
+from typing import Dict, List, Optional
+from warnings import warn
 import pandas as pd
 import numpy as np
 
+import scipy.stats
+
+from bank_schedule.data import Data
+from bank_schedule.forecast import load_model, LGBM_MODEL_NAME
+
 INCOME_COL = 'money_in'
 RESIDUALS_COL = 'money'
+DATE_COL = 'date'
+
+# количество дней до переполнения банкомата
+# или до 14 дней без обслуживания
+DAYS_TO_DEADLINE_COL = 'days_to_deadline'
+
+# дата прошлого посещения банкомата инкассаторами
+LAST_COLLECTION_DATE_COL = 'last_collection_date'
+
+# дата, начиная с которой нам известны данные об остатках
+INITIAL_DATE = '2022-08-31'
+
+
+def get_initial_resuduals(loader: Data,
+                          initial_date: str=INITIAL_DATE,
+                          last_collection_equal_to_initial: bool=True):
+    """_summary_
+
+    Args:
+        loader (Data): _description_
+        initial_date (str, optional): _description_. Defaults to INITIAL_DATE.
+    """
+    residuals = loader.get_money_start()
+    incomes = loader.get_money_in()
+
+    residuals[DATE_COL] = pd.to_datetime(initial_date)
+    residuals = add_last_cash_collection_date(
+        residuals, incomes, equal_to_initial=last_collection_equal_to_initial
+        )
+
+    return residuals
+
+
+def prepare_residual_to_schedule_creation(residuals: pd.DataFrame,
+                                          forecast_model: object,
+                                          horizon: int) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        residuals (pd.DataFrame): _description_
+        forecast_model (object): _description_
+        horizon (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    residuals = add_nearest_overflow_date(residuals,
+                                          forecast_model=forecast_model,
+                                          horizon=horizon)
+
+    residuals = calc_and_add_days_to_deadline_column(residuals)
+
+    return residuals
 
 
 def get_today_from_residuals(residuals: pd.DataFrame) -> pd.Timestamp:
@@ -22,22 +81,43 @@ def get_today_from_residuals(residuals: pd.DataFrame) -> pd.Timestamp:
     Returns:
         pd.Timestamp: _description_
     """
-    today = residuals['date'].unique()
+    today = residuals[DATE_COL].unique()
     if today.shape[0] > 1:
         raise ValueError('More than one unique date in the residuals dataframe')
     return pd.to_datetime(today[0])
 
 
-def add_overflow_date(today_residuals: pd.DataFrame,
-                      forecast_model: object,
-                      horizon: int=3,
-                      overflow_thresh: int=10**6) -> List[int]:
-    """Проставляем даты переполнения банкоматов, если оно произойдет
+def calc_and_add_days_to_deadline_column(residuals: pd.DataFrame) -> pd.DataFrame:
+    """Добавляет колонку days_to_deadline
+
+    Args:
+        residuals (pd.DataFrame): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+
+    residuals = residuals.copy()
+    days_to_collecting = 14 - (residuals[DATE_COL] - residuals['last_collection_date']).dt.days
+    days_to_overflow_thresh = (residuals['overflow_date'] - residuals[DATE_COL]).dt.days
+
+    residuals[DAYS_TO_DEADLINE_COL] = list(map(min, days_to_collecting, days_to_overflow_thresh))
+    residuals[DAYS_TO_DEADLINE_COL] = residuals[DAYS_TO_DEADLINE_COL].astype(int)
+
+    return residuals
+
+
+def add_nearest_overflow_date(today_residuals: pd.DataFrame,
+                              forecast_model: object,
+                              horizon: int=3,
+                              overflow_thresh: int=10**6) -> List[int]:
+    """Проставляем ближайшие ожидаемые даты переполнения банкоматов, если оно произойдет
      в течение horizon дней
     1. Прогнозируем income на horizon дней вперед
     2. На основе прогноза определяем даты переполнения банкоматов
      в пределах заданного горизонта horizon
     3. Проставляем даты переполнения в рамках горизонта
+    Предполагается, что мы делаем это вечером после инкассации
 
     Args:
         today_residuals (pd.DataFrame): данные об остатках на текущий вечер
@@ -63,7 +143,7 @@ def add_overflow_date(today_residuals: pd.DataFrame,
     in_preds.set_index('TID', inplace=True)
 
     # извлекаем даты, на которые сделали прогноз
-    predicted_dates = np.sort(in_preds['date'].unique())
+    predicted_dates = np.sort(in_preds[DATE_COL].unique())
 
     # даты предполагаемых переполнений банкоматов
     today_residuals['overflow_date'] = pd.NaT
@@ -82,7 +162,7 @@ def add_overflow_date(today_residuals: pd.DataFrame,
             break
 
         # прогнозы пополнений за текущую дату
-        date_in_preds = in_preds[in_preds['date'] == date].copy()
+        date_in_preds = in_preds[in_preds[DATE_COL] == date].copy()
         date_in_preds = date_in_preds.loc[today_residuals.index, :]
 
         # пополнение
@@ -97,7 +177,7 @@ def add_overflow_date(today_residuals: pd.DataFrame,
 
 def add_last_cash_collection_date(residuals: pd.DataFrame,
                                   income: pd.DataFrame,
-                                  fake: bool=True) -> pd.DataFrame:
+                                  equal_to_initial: bool=True) -> pd.DataFrame:
     """Считает ориентировочную дату последней инкассации
     на момент 2022-08-31 исходя из средней заполняемости банкомата в день
 
@@ -105,8 +185,8 @@ def add_last_cash_collection_date(residuals: pd.DataFrame,
         residuals (pd.DataFrame): _description_
         income (pd.DataFrame): _description_
     """
-    if fake:
-        residuals['last_collection_date'] = pd.to_datetime('2022-08-31')
+    if equal_to_initial:
+        residuals['last_collection_date'] = pd.to_datetime(INITIAL_DATE)
         return residuals
 
     avg_day_income = income.groupby('TID')[INCOME_COL].mean()
@@ -117,14 +197,14 @@ def add_last_cash_collection_date(residuals: pd.DataFrame,
     days_from_last_collection = np.ceil(residuals_series / avg_day_income).astype(int)
     days_from_last_collection = days_from_last_collection.apply(lambda x: min(x, 14))
 
-    residuals['last_collection_date'] = residuals['date'] - \
+    residuals['last_collection_date'] = residuals[DATE_COL] - \
         pd.to_timedelta(days_from_last_collection, unit='D')
 
     return residuals.reset_index()
 
 
 def calc_label_to_weights_map(data: pd.DataFrame,
-                              label_column: str='days_to_deadline'
+                              label_column: str=DAYS_TO_DEADLINE_COL
                               ) -> Dict[str, int]:
     """Возвращает словарь с весами классов для выборки их из данных
     Веса нужны, чтобы выбирать сбалансированный случайный сэмпл
@@ -135,18 +215,21 @@ def calc_label_to_weights_map(data: pd.DataFrame,
          окончания срока без обслуживания
         label_column (str, optional): колонка с количеством дней до обязательной
          инкассации банкомата.
-        Defaults to 'days_to_deadline'.
+        Defaults to DAYS_TO_DEADLINE_COL.
 
     Returns:
         Dict[str, int]: _description_
     """
 
-    n_samples_by_classes = data[label_column].value_counts()
-    n_samples = n_samples_by_classes.sum()
-    n_classes = n_samples_by_classes.shape[0]
-    classes_weights = n_samples / (n_classes * n_samples_by_classes)
-    classes_weights = classes_weights.to_dict()
-    return classes_weights
+    mynnorm = scipy.stats.norm(0, 7)
+    return {i: mynnorm.pdf(i) for i in range(100)}
+
+    # n_samples_by_classes = data[label_column].value_counts()
+    # n_samples = n_samples_by_classes.sum()
+    # n_classes = n_samples_by_classes.shape[0]
+    # classes_weights = n_samples / (n_classes * n_samples_by_classes)
+    # classes_weights = classes_weights.to_dict()
+    # return classes_weights
 
 
 def calc_samples_weights(data: pd.DataFrame,
@@ -164,10 +247,28 @@ def calc_samples_weights(data: pd.DataFrame,
     return data[label_column].map(base_to_weights_map).to_list()
 
 
+def get_weights_from_residuals(residuals: pd.DataFrame,
+                               std: float=7.0) -> Dict:
+    """Получает веса для банкоматов по их близости к дедлайну
+    (по обслуживанию и переполнению)
+
+    Args:
+        residuals (pd.DataFrame): _description_
+
+    Returns:
+        Dict: _description_
+    """
+    weights_base = residuals[['TID', 'days_to_deadline']].drop_duplicates()
+    mynnorm = scipy.stats.norm(0, std)
+    weights_base['weight'] = weights_base['days_to_deadline'].apply(mynnorm.pdf)
+    return weights_base.set_index('TID')['weight'].to_dict()
+
+
 def get_non_mandatory_atms_samples(data: pd.DataFrame,
                                    n_samples: int,
                                    mandatory_selection_threshold: int,
-                                   mandatory_selection_col: str='days_to_deadline') -> pd.DataFrame:
+                                   mandatory_selection_col: str=DAYS_TO_DEADLINE_COL,
+                                   tids_list: Optional[List[int]]=None) -> pd.DataFrame:
     """Отбирает для инкассации на сегодня банкоматы, которые не обязательно
     обслуживать именно сегодня, но если обслужим - сбалансируем выборку
 
@@ -175,20 +276,37 @@ def get_non_mandatory_atms_samples(data: pd.DataFrame,
         data (pd.DataFrame): _description_
         n_samples (int): _description_
         mandatory_selection_threshold (int): _description_
-        mandatory_selection_col (str, optional): _description_. Defaults to 'days_to_deadline'.
-        tids_col (str, optional): _description_. Defaults to 'TID'.
+        mandatory_selection_col (str, optional): _description_. Defaults to DAYS_TO_DEADLINE_COL.
+        tids_list (Optional[List[int]], optional): среди каких банкоматов выбирать.
+         Defaults to None.
 
     Returns:
         pd.DataFrame: _description_
     """
     non_mandatory = data[data[mandatory_selection_col] > mandatory_selection_threshold].copy()
-    weights = calc_samples_weights(non_mandatory, mandatory_selection_col)
-    return non_mandatory.sample(n=n_samples, weights=weights)
+    non_mandatory['weights'] = calc_samples_weights(non_mandatory, mandatory_selection_col)
+
+    if tids_list is not None:
+        non_mandatory = non_mandatory[non_mandatory['TID'].isin(tids_list)].copy()
+
+    total_samples = non_mandatory.shape[0]
+
+    if n_samples > total_samples:
+        warn(f'В выборке {total_samples} точек. Это меньше, чем {n_samples}. '
+             'Всё, что есть, будет отобрано.')
+        n_samples = total_samples
+
+    non_mandatory = non_mandatory.sample(n=n_samples,
+                                         weights='weights',
+                                         replace=False,
+                                         random_state=0)
+
+    return non_mandatory.drop(columns='weights')
 
 
 def get_mandatory_samples(data: pd.DataFrame,
                           mandatory_selection_threshold: int,
-                          mandatory_selection_col: str='days_to_deadline') -> pd.DataFrame:
+                          mandatory_selection_col: str=DAYS_TO_DEADLINE_COL) -> pd.DataFrame:
     """Возвращает срез данных с банкоматами, которые обязательно обслужить сегодня
     (сегодня вечером будут переполнены или сегодня - 14 день без обслуживания
 
@@ -196,39 +314,142 @@ def get_mandatory_samples(data: pd.DataFrame,
         data (pd.DataFrame): _description_
         n_samples (int): _description_
         mandatory_selection_threshold (int): _description_
-        mandatory_selection_col (str, optional): _description_. Defaults to 'days_to_deadline'.
+        mandatory_selection_col (str, optional): _description_. Defaults to DAYS_TO_DEADLINE_COL.
     """
     return data[data[mandatory_selection_col] <= mandatory_selection_threshold].copy()
 
 
-def get_atms_for_today_collection(data: pd.DataFrame,
+def get_atms_for_today_collection(loader: Data,
+                                  residuals: pd.DataFrame,
                                   n_samples: int,
                                   mandatory_selection_threshold: int,
-                                  mandatory_selection_col: str='days_to_deadline',
-                                  tids_col: str='TID') -> pd.DataFrame:
-    """Возвращает срез даннки с банкоматама, коллежирующимим для обслуживания
-    текущимиддельникоматама
+                                  neighborhood_radius: 15,
+                                  mandatory_selection_col: str=DAYS_TO_DEADLINE_COL,
+                                  tids_col: str='TID',
+                                  use_greedy: bool=True) -> pd.DataFrame:
+    """Возвращает срез датафрейма остатков с банкоматами, которые нукжно сегодня инкассировать
 
     Args:
-        data (pd.DataFrame): _description_
-        n_samples (int): _description_
-        mandatory_selection_threshold (int): _description_
-        mandatory_selection_col (str, optional): _description_. Defaults to 'days_to_deadline'.
-        tids_col (str, optional): _description_. Defaults to 'TID'.
+        residuals (pd.DataFrame): остатки в банкоматах на вчерашний вечер
+        n_samples (int): сколько банкоматов мы должны сегодня инкассировать
+        mandatory_selection_threshold (int): банкоматы с каким количеством дней до дедлайна
+         мы должны обязательно инкассировать сегодня
+        mandatory_selection_col (str, optional): в какой колонке лежит рассчитанное количество
+         дней до дедлайна. Defaults to DAYS_TO_DEADLINE_COL.
+        tids_col (str, optional): колонка с айди банкоматов. Defaults to 'TID'.
 
     Returns:
-        pd.DataFrame: _description_
+        pd.DataFrame: срез датафрейма остатков с банкоматами, которые нукжно сегодня инкассировать
     """
-    mandatory = get_mandatory_samples(data, mandatory_selection_threshold,
+    mandatory = get_mandatory_samples(residuals, mandatory_selection_threshold,
                                       mandatory_selection_col)
+    mandatory['is_mandatory'] = 1
+
     n_samples_non_mandatory = n_samples - mandatory.shape[0]
-    non_mandatory = get_non_mandatory_atms_samples(data,
-                                                   n_samples_non_mandatory,
-                                                   mandatory_selection_threshold,
-                                                   mandatory_selection_col)
+
+    if n_samples_non_mandatory <= 0:
+        warn(
+            f'Банкоматов, обязательных для обслуживания - {mandatory.shape[0]} (> {n_samples}). '
+            'Будут инкассироваться только они, соседние не добавляем.'
+            )
+
+        if mandatory.duplicated(subset=tids_col).any():
+            raise ValueError('Дублируются банкоматы в датафрейме остатков')
+
+        return mandatory
+
+    neighbours_list = get_neighbours(mandatory[tids_col].to_list(),
+                                     loader,
+                                     radius=neighborhood_radius)
+    if use_greedy:
+        non_mandatory = get_atms_for_today_collection_greedy(residuals,
+                                                             n_samples_non_mandatory,
+                                                             mandatory_selection_threshold,
+                                                             tids_list=neighbours_list)
+    else:
+        non_mandatory = get_non_mandatory_atms_samples(residuals,
+                                                       n_samples_non_mandatory,
+                                                       mandatory_selection_threshold,
+                                                       mandatory_selection_col,
+                                                       tids_list=neighbours_list)
+
+    non_mandatory['is_mandatory'] = 0
+
     atms_df = pd.concat([non_mandatory, mandatory], axis=0, ignore_index=True)
 
     if atms_df.duplicated(subset=tids_col).any():
         raise ValueError('Дублируются банкоматы в датафрейме остатков')
 
     return atms_df
+
+
+def get_neighbours(tids_list: List[int],
+                   loader: Data,
+                   radius: float=15,
+                   ) -> List[int]:
+    """Получает соседние банкоматы для банкоматов из tids_list
+
+    Args:
+        tids_list List[int]: _description_
+        loader (Data): _description_
+        radius (float, optional): _description_. Defaults to 15.
+
+    Returns:
+        List[int]: _description_
+    """
+    tids_set = set(tids_list)
+
+    distances_df = loader.get_distance_matrix()
+
+    cond1 = distances_df['Origin_tid'].isin(tids_set)
+    cond2 = distances_df['Destination_tid'].isin(tids_set)
+
+    time_cond = distances_df['Total_Time'] < radius
+
+    nb_set1 = set(distances_df.loc[cond1 & time_cond, 'Destination_tid'])
+    nb_set2 = set(distances_df.loc[cond2 & time_cond, 'Origin_tid'])
+
+    result = nb_set1.union(nb_set2)
+
+    return list(result.difference(tids_set))
+
+def get_atms_for_today_collection_greedy(residuals: pd.DataFrame,
+                                         n_samples: int=200,
+                                         mandatory_selection_threshold: int=1,
+                                         tids_list: Optional[List[int]]=None
+                                         ):
+    """Жадно отбирает банкоматы для инкассации
+
+    Args:
+        residuals (pd.DataFrame): _description_
+        n_samples (int, optional): _description_. Defaults to 200.
+    """
+    cond = [True for _ in range(residuals.shape[0])]
+
+    if tids_list is not None:
+        tids_list = set(tids_list)
+        cond = residuals['TID'].isin(tids_list)
+
+    counts_by_days = residuals.loc[cond, 'days_to_deadline'].value_counts().sort_index().to_dict()
+
+    free_space = n_samples
+    to_collect_list = []
+
+    for i, cnt in counts_by_days.items():
+        if free_space <=0:
+            break
+
+        to_collect_cnt = min(cnt, free_space)
+
+        if i <= mandatory_selection_threshold:
+            to_collect_cnt = cnt
+
+        df = residuals[residuals['days_to_deadline']==i].sample(n=to_collect_cnt, random_state=0)
+        df['is_mandatory'] = int(i <= mandatory_selection_threshold)
+
+        to_collect_list.append(df.copy())
+        free_space -= to_collect_cnt
+    if free_space > 0:
+        warn(f'В выборке {n_samples - free_space} точек. Это меньше, чем {n_samples}. '
+             'Всё, что есть, будет отобрано.')
+    return pd.concat(to_collect_list, ignore_index=True)
